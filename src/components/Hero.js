3ac107805a13547852662892257e87ee5ff74606
Tristverm <emmanuelkyalo72@gmail.com>
2023-08-05 22:33:09 +0300

    Removed importation bug

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -11,14 +11,17 @@ import { BsFillSunFill, BsMoonFill, BsCheck } from "react-icons/bs";
 //importing images
 import GirlImg from "../assets/img/girl.png";
 
+
 function Hero() {
   //  updating theme using state
-  const[(theme, setTheme)] = useState("light");
+  const [theme, setTheme] = useState("light");
 
   return (
     <section className="min-h-[740px] w-full bg-heroLight bg-cover bg-center bg-no-repeat overflow-hidden dark:bg-heroDark ">
       {/* buttton */}
-      button.p-4.bg-accent.text-white.rounded-full.w-12.h-12.flex.justify-center
+      <button className="p-4 bg-accent text-white rounded-full w-12 h-12 flex justify-center items-center">
+        {theme === "light" ? <BsMoonFill /> : <BsFillSunFill />}
+      </button>
     </section>
   );
 }
