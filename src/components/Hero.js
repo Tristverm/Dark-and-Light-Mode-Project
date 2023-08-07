405705b167de668cf870f488e3e9654f18551a33
Tristverm <emmanuelkyalo72@gmail.com>
2023-08-07 22:09:02 +0300

    added added useeffect

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -14,20 +14,29 @@ import GirlImg from "../assets/img/girl.png";
 function Hero() {
   // setting the theme state
   const [theme, setTheme] = useState("light");
+  // finding html element reference
+  const htmlRef = document.querySelector("#html");
 
   //theme switch handler
   function themeSwitchHandler() {
-    // finding html element reference
-    const htmlRef = document.querySelector("#html");
     // condition
     if (theme === "light") {
       setTheme("dark");
       htmlRef.classList.add("dark");
+      localStorage.clear();
+      localStorage.setItem("theme", "dark");
     } else {
       setTheme("light");
       htmlRef.classList.remove("dark");
+      localStorage.clear();
+      localStorage.setItem("theme", "light");
     }
   }
+  useEffect(() => {
+    if (localStorage.getItem(theme) === null) {
+      htmlRef.classList.add("dark");
+    }else
+  });
   return (
     <section className="min-h-[740px] w-full bg-heroLight bg-cover bg-center bg-no-repeat overflow-hidden dark:bg-heroDark ">
       {/* buttton */}
